c52eb244ea7f54186324db4d46653ac53b457bef
leandre Blanchard <leandre.blanchard@eptiech.eu>
2017-11-03 23:18:34 +0100

    fix return main erreur

--- src/main.c
+++ src/main.c
@@ -26,7 +26,12 @@ int		main(int ac, char **av)
   window->font = my_strdup(FONT);
   folder.folder = av[1];
   if (create_frames(&frames, &folder) == -1)
+  {
     mdprintf(2, "%sError : Could not create frames%s\n", BOLDRED, RESET);
+    free_tab(&folder.filenames);
+    free_window(window);
+    return (-1);
+  }
   menu(window, frames, &folder);
   free_frames(frames, folder.size);
   free_tab(&folder.filenames);
